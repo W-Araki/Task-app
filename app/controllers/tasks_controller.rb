e9a059c918cdf45aacb227fbc79b854e97ae8ccd
W-Araki <aino18nichidai18@gmail.com>
2019-10-10 10:25:07 +0000

    tasks-html

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,21 @@
 class TasksController < ApplicationController
+  before_action :set_user
   
   def index
+  end
+  
+  def new
+  end
+  
+  def show
+  end
+  
+  def edit
+  end
+  
+  private
+  
+  def set_user
+    @user = User.find(params[:user_id])
   end
 end
