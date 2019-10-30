4fa6ca4c77312efda43d58feaa56f0a1d3c0243c
W-Araki <aino18nichidai18@gmail.com>
2019-10-30 04:28:30 +0000

    Finish! task-app

--- app/models/task.rb
+++ app/models/task.rb
@@ -5,5 +5,5 @@ class Task < ApplicationRecord
   
   validates :user_id, presence: true
   validates :name, presence: true, length: { maximum: 50 }
-  validates :description, presence: true, length: { in: 5..300 }
+  validates :description, presence: true, length: { in: 5..500 }
 end
